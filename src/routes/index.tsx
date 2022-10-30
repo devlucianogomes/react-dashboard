10997eea5a4f4e39f21546a97e6df5d2e5ba0a46
luciano <luciano.gomes@prestadores.amil.com.br>
2022-10-30 19:05:42 -0300

    feat: implementation of the initial routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/home" element={<h1>Dashboard</h1>} />
+      <Route path="*" element={<Navigate to="/home" />} />
+    </Routes>
+  );
+};
